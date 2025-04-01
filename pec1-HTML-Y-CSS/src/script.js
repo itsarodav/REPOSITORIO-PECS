f942c42af3ddd8f41727954b207b1afd1e73ac2f
itsarodav <82170461+itsarodav@users.noreply.github.com>
2025-04-01 02:04:38 +0200

    Añadiendo nuevos archivos y corrigiendo errores de header

--- pec1-HTML-Y-CSS/src/script.js
+++ pec1-HTML-Y-CSS/src/script.js
@@ -5,6 +5,7 @@ const closeMenu = document.getElementById('close-menu');
 const navbar = document.getElementById('navbar');
 const ddCategoria = document.querySelector(".submenu > a");
 const dropdown = document.querySelector(".submenu .dropdown");
+const categoriaLink = document.getElementById("dd-selection");
 
 menuToggle.addEventListener('click', () => {
     navbar.classList.add('active');
@@ -17,4 +18,19 @@ closeMenu.addEventListener('click', () => {
 ddCategoria.addEventListener("click", (event) => {
     event.preventDefault(); // Evita que el enlace redirija
     dropdown.classList.toggle("active");
+});
+
+function isMobile() {
+    return window.innerWidth < 768; // o tu breakpoint
+}
+
+    categoriaLink.addEventListener("click", function (e) {
+    if (isMobile()) {
+        if (!dropdown.classList.contains("active")) {
+        // Primer click: abre el submenu
+        e.preventDefault();
+        dropdown.classList.add("active");
+        } 
+      // Segundo click: no se hace preventDefault, va al enlace normalmente
+    }
 });
